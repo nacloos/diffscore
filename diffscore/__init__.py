7f057b2eac17c6229e1738d6c828b9a1c36c932b
nacloos <cloosnathan@gmail.com>
2024-03-10 18:14:28 -0500

    Update __init__.py

--- diffscore/__init__.py
+++ diffscore/__init__.py
@@ -7,6 +7,7 @@ from diffscore import training as training
 # TODO: rename model to nn?
 from diffscore import model as model
 from diffscore import dataset as dataset
+from diffscore import env as env
 
 from diffscore.analysis import Measure
 from diffscore.env import Env
