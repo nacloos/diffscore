c5dfe8551f404c2dbc1a28678c29d280b60379dc
nacloos <cloosnathan@gmail.com>
2024-03-13 10:52:20 -0400

    Update __init__.py

--- diffscore/__init__.py
+++ diffscore/__init__.py
@@ -2,13 +2,22 @@ import similarity
 # temp use registry from similarity
 from similarity import make, register
 
-from diffscore import analysis as analysis
-# TODO: rename model to nn?
-from diffscore import model as model
-from diffscore import dataset as dataset
-from diffscore import env as env
-# TODO: have to import training after env because use env in training?
-from diffscore import training as training
+from . import analysis
+from . import model
+from . import dataset
+from . import env
+from . import training
 
-from diffscore.analysis import Measure
-from diffscore.env import Env
+from .analysis import Measure
+from .env import Env
+
+# from diffscore import analysis as analysis
+# # TODO: rename model to nn?
+# from diffscore import model as model
+# from diffscore import dataset as dataset
+# from diffscore import env as env
+# # TODO: have to import training after env because use env in training?
+# from diffscore import training as training
+
+# from diffscore.analysis import Measure
+# from diffscore.env import Env
