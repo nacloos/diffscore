770148bb45885128ee716f00fcd089c3928abc3c
nacloos <cloosnathan@gmail.com>
2024-03-17 19:04:09 -0400

    add env

---
+++ diffscore/env/__init__.py
@@ -0,0 +1,9 @@
+# TODO: register envs
+
+import similarity
+
+
+# TODO: temp use similarity registry
+# class Env:
+#     def __new__(cls, env_id: str, *args, **kwarg):
+#         return similarity.make(f"env.diffscore.{env_id}", *args, **kwargs)
