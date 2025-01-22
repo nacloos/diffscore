59b8f946495f941bdd262d19d6f6906c7b6d5b12
nacloos <cloosnathan@gmail.com>
2025-01-21 22:10:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     install_requires=[
         'pytorch_lightning',
         'neurogym==0.0.2',
+        'numpy<2.0.0',
         # 'brainscore_vision @ git+https://github.com/brain-score/vision',
         'similarity-repository @ git+https://github.com/nacloos/similarity-repository.git'
     ],
